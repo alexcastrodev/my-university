2477542d1774b1b03fa3842009d64e6e24cf38b4
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-26 14:50:07 +0100

    fix: change topic column type to varchar for consistency

--- backend/src/lesson/lesson.entity.ts
+++ backend/src/lesson/lesson.entity.ts
@@ -24,7 +24,7 @@ export class Lesson {
   @Column('text', { nullable: true })
   contentPath: string | null;
 
-  @Column({ nullable: true })
+  @Column('varchar', { nullable: true })
   topic: string | null;
 
   @ManyToOne(() => CourseModule, (m) => m.lessons, { onDelete: 'CASCADE' })
